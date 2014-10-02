430d824d5c34614bfea0c5fba68d94949a7c4a7b
gbales11 <gbalestra@hotmail.ca>
2014-10-02 10:59:09 -0400

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+var salesdetailsleft = $('.sales__detailsleft');
+/* the var means youre making a box, sales details left is what the box is called. .sales__detailsleft is inside the box (what youre getting the class that you put on the html)*/
+
+var salesleft = $('.salesleft'); 
+
+var buttonClickHandler = function () {
+	
+	var currentState = salesdetailsleft.attr('data-state');
+	
+	if (currentState == 'active'){ 
+		salesdetailsleft.attr('data-state', 'inactive')
+	} else {
+		salesdetailsleft.attr('data-state', 'active')
+	}	
+};
+
+salesleft.on('click', buttonClickHandler);
