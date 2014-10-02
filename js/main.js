a0c8e41a5a22091140c52c6ec36e7e9178e3ca04
gbales11 <gbalestra@hotmail.ca>
2014-10-02 11:22:20 -0400

    added main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,22 @@ var buttonClickHandler = function () {
 };
 
 salesleft.on('click', buttonClickHandler);
+
+
+var salesdetailsright = $('.sales__detailsright');
+/* the var means youre making a box, sales details left is what the box is called. .sales__detailsleft is inside the box (what youre getting the class that you put on the html)*/
+
+var salesright = $('.salesright'); 
+
+var buttonClickHandler = function () {
+	
+	var currentState = salesdetailsright.attr('data-state');
+	
+	if (currentState == 'active'){ 
+		salesdetailsright.attr('data-state', 'inactive')
+	} else {
+		salesdetailsright.attr('data-state', 'active')
+	}	
+};
+
+salesright.on('click', buttonClickHandler);
